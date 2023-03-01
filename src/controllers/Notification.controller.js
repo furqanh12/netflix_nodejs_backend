5105e60b529be538ed444eeb73135442f433849c
Furqan Haroon <furqan@isoftstudios.com>
2023-03-01 18:36:18 +0500

    notification work complete

--- src/controllers/Notification.controller.js
+++ src/controllers/Notification.controller.js
@@ -8,8 +8,8 @@ class NotificationsController{
             try {
              const user = await UserSechema.findOne({_id:req.user_id}).populate('notifications.movie')
                     if (user) {
-                    const notifications = user.notifications;
-                    return res.send(notifications)
+                        const notifications = user.notifications.filter(notification => notification.mark_as_read === false);
+                        return res.send(notifications);
                     } else {
                         return res.status(404).send({message:'No User Found'})
                     }
@@ -19,6 +19,35 @@ class NotificationsController{
             }
         }
     }
+
+    set_read_notifications(){
+        return async (req, res) => {
+            const {message} = req.body
+            const userId = req.user_id
+            try {
+             const user = await UserSechema.updateOne(
+                {
+                  _id: userId,
+                  notifications: {
+                    $elemMatch: {
+                      message: message,
+                      mark_as_read: false
+                    }
+                  }
+                },
+                {
+                  $set: { 'notifications.$.mark_as_read': true }
+                }
+              );
+              console.log(user);
+              res.send(user)
+            } catch (error) {
+                console.log(error);
+                res.send(error)
+            }
+        }
+    }
+
 }
 
 exports.NotificationsController = NotificationsController;
--- src/routes/Notification.routes.js
+++ src/routes/Notification.routes.js
@@ -9,5 +9,6 @@ const notification = new NotificationsController;
 
 
 router.get('/all_notifications',AuthToken(),notification.all_notification())
+router.post('/set_read_notifications',AuthToken(),notification.set_read_notifications())
 
 exports.router = router;
